172f83b9554a3bb849debf73e829a120aa7d0234
Fernnandoo <116328071+Fernnandoo@users.noreply.github.com>
2023-09-30 14:35:30 -0300

    Update index.js

--- src/pages/Entregador/AceitarEntrega/index.js
+++ src/pages/Entregador/AceitarEntrega/index.js
@@ -1,26 +1,85 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function AceitarEntrega() {
- return (
-    <View style={styles.container}>
-        <View style={styles.header}>
-                <TouchableOpacity style={styles.headerBell}><Icon name='notifications' size={30} color='#ffc000' /></TouchableOpacity>
-            </View>
+    const [order, setOrder] = useState("1 X-salada\n1 Coca cola");
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        { label: 'Dinheiro', value: 'java' },
+        { label: 'Cartão débito ou crédito', value: 'cartao' },
+        { label: 'Pix', value: 'pix' }
+    ]);
 
-            <View style={styles.pedidos}>
-                <Text style={styles.pedidosText}>Entregas pendentes</Text>
-                <View style={styles.pedidosClock}>
-                    <Icon name='time-outline' size={30} color='#000' />
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity style={styles.headerBell}><Icon name='notifications' size={30} color='#ffc000' /></TouchableOpacity>
                 </View>
+
+
+                <View style={styles.pedidos}>
+                    <Text style={styles.pedidosText}>Entregas pendentes</Text>
+                    <View style={styles.pedidosClock}>
+                        <Icon name='time-outline' size={30} color='#000' />
+                    </View>
+                </View>
+                <View style={styles.card}>
+                    <View style={styles.recentsContent}>
+                        <View style={styles.recentsImages}>
+                        </View>
+                        <Text>
+                            Luzia Hamburgers {'\n'}
+                            Cd pedido: 01
+                        </Text>
+                    </View>
+                    <View style={styles.recentsContent}>
+                        <View style={styles.recentsImages}>
+                        </View>
+                        <Text>
+                            Nome do cliente {'\n'}
+                            João Alvero Santos Lula
+                        </Text>
+                    </View>
+                    <View style={styles.comanda}>
+                        <Text style={styles.comandaTitle}>Comanda do pedido:</Text>
+                        <View style={styles.comandaDescription}>
+                            <TextInput
+                                multiline={true}
+                                onChangeText={text => setOrder(text)}
+                                numberOfLines={4}
+                                value={order}
+                            />
+                        </View>
+                        <Text style={styles.comandaTitle}>Forma de pagamento:</Text>
+                        <View style={styles.comandaPayment}>
+                            <DropDownPicker
+                                open={open}
+                                value={value}
+                                items={items}
+                                setOpen={setOpen}
+                                setValue={setValue}
+                                setItems={setItems}
+                                style={{ borderColor: 'transparent' }}
+                                dropDownContainerStyle={{ borderColor: 'transparent' }}
+                            />
+                        </View>
+                        <Text style={styles.comandaTitle}>Valor do pedido:</Text>
+                        <View style={styles.comandaPaymentValue}>
+                            <Text>R$ 15,99</Text>
+                        </View>
+                    </View>
+                </View>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Iniciar entrega</Text>
+                </TouchableOpacity>
             </View>
-            <View style={styles.card}>
-                <Text>hello wolrd</Text>
-            </View>
-    </View>
-  );
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -52,7 +111,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
     },
-     pedidos: {
+    pedidos: {
         flexDirection: 'row',
         alignContent: 'center',
         alignItems: 'center',
@@ -68,9 +127,74 @@ const styles = StyleSheet.create({
     },
     card: {
         backgroundColor: '#FFC000',
-        height: '60%',
+        height: 'fit-content',
         width: '90%',
-        borderRadius: 20,
-        padding: 15
-    }
+        borderRadius: 8,
+        padding: 10
+    },
+    recentsContent: {
+        flexDirection: 'row',
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        elevation: 2,
+        padding: 20,
+        width: 320,
+        height: 80,
+        borderRadius: 10,
+        alignContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20
+    },
+    recentsImages: {
+        borderRadius: 50,
+        backgroundColor: '#fff',
+        width: 70,
+        height: 70,
+        marginRight: 10,
+    },
+    comanda: {
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        elevation: 2,
+        padding: 20,
+        width: 320,
+        height: 'fit-content',
+        borderRadius: 10,
+        marginBottom: 20
+    },
+    comandaDescription: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 10,
+        height: 'fit-content',
+        padding: 10,
+    },
+    comandaPayment: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 10,
+        height: 40,
+        marginBottom: 'fit-content'
+    },
+    comandaPaymentValue: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 10,
+        height: 40,
+        padding: 10
+    },
+    comandaTitle: {
+        marginBottom: 5,
+        marginTop: 10,
+    },
+    button: {
+        height: 50,
+        width: 250,
+        backgroundColor: '#ffc000',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginTop: 10,
+        elevation: 2
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: '600',
+    },
 })
