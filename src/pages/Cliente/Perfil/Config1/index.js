162f34fa8789ebebac248326851b4712c63ab914
Fernnandoo <luizamuirim7@gmail.com>
2023-10-09 14:16:00 -0300

    att

--- src/pages/Cliente/Perfil/Config1/index.js
+++ src/pages/Cliente/Perfil/Config1/index.js
@@ -26,8 +26,11 @@ export default function Config() {
                     onPress={() => navigation.pop(1)}>
                     <Icon name='chevron-back' size={30} color='#000' />
                 </TouchableOpacity>
+                <View style={{flexDirection: 'row'}}>
                 <Text style={styles.configText}>Configurações</Text>
                 <Icon name='cog' size={30} color={tema.color} />
+                </View>
+                <View/>
             </View>
             <View style={styles.btnArea}>
                 <Text style={styles.configContent}>
@@ -75,18 +78,18 @@ const getstyles = (tema) => StyleSheet.create({
     config: {
         marginTop: 50,
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 15,
+        justifyContent: 'space-around',
+        paddingLeft: 15,
         alignItems: 'center',
-        width: '100%'
+        width: '100%',
     },
-    chevron: {
-        marginRight: 100
-    },
+    // chevron: {
+    //     alignSelf: 'flex-start'
+    // },
     configText: {
         fontSize: 20,
         marginRight: 15,
-        color: tema.Tema.color
+        color: tema.Tema.color,
     },
     configContent: {
         fontSize: 18,
